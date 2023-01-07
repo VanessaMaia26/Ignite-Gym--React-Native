58b76f1aa0800b17caa01559caf190af2bd41a1a
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-01-07 16:57:44 -0300

    new commit

---
+++ src/screens/Home.tsx
@@ -0,0 +1,34 @@
+
+import { VStack, FlatList } from "native-base";
+import { useState } from "react";
+
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+
+export function Home() {
+    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro']);
+    const [groupSelected, setGroupSelected] = useState('costa');
+
+    return (
+        <VStack flex={1} >
+            <HomeHeader />
+
+            <FlatList
+               data={groups}
+               keyExtractor={item => item}
+               renderItem={({ item }) => (
+                <Group
+                   name={item}
+                   isActive={groupSelected === item}
+                   onPress= {() => setGroupSelected(item)}
+                />
+               )}
+               horizontal
+               showsHorizontalScrollIndicator={false}
+               _contentContainerStyle={{px: 8}}
+               my={10}
+               maxH={10}
+            />
+        </VStack>
+    );
+}
